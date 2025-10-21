c0b841aa7f895803a4647bf9e9059133356559c4
naz <merveien@yandex.com>
2025-10-21 15:02:18 +0300

    Add files via upload

---
+++ sfgdfhgjj.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath> // matematik fonksiyonları için "genelde"
+using namespace std;
+double x = 3.9 ; 
+int y = 34 ;
+int main () {
+cout << log (x); //10luk değil e tabanında log alır 
+
+}
